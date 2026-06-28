9d7f475abaaa2a6a92b81cda608d4e9546d77e87
TechShooter <cloud019998@gmail.com>
2026-06-28 18:42:03 +0200

    css fix

--- app/components/WorkspaceManager.tsx
+++ app/components/WorkspaceManager.tsx
@@ -1807,7 +1807,7 @@ export default forwardRef(function WorkspaceManager({ notes, aiModel, sheetData,
         </div>
 
         {/* Tab Content */}
-        <div className="flex-1 overflow-hidden min-w-0 min-h-0 sm:min-w-[800px]">
+        <div className="flex-1 overflow-hidden min-w-0 min-h-0 sm:min-w-[800px] flex flex-col">
           {activeTab === 'chat' && currentWorkspace.id === 'model-testing' && (
             <Suspense fallback={<div className="text-white p-4">Loading Model Testing...</div>}>
               <ModelTesting
